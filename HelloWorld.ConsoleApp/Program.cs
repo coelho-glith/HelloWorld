c2b28fc56765329a0e71f577838e67c63564a86e
coelho-glith <coelho.soldado7@gmail.com>
2025-03-06 15:34:12 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWorld.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorld.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Hello, World!");
+
+          Console.WriteLine("bom dia galerinha");
+
+          Console.ReadLine(); 
+        }
+    }
+}
